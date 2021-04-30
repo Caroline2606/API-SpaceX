d1d804b7b878e42df894691752777752c31acadc
Caroline2606 <karolina.miedzianowska@onet.pl>
2021-04-30 17:33:47 +0200

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
+
 import requests
 import csv
 
 response = requests.get('https://api.spacexdata.com/v3/launches')
-data = response.json()
+data = requests.get('https://api.spacexdata.com/v3/launches').json()
+
+
+class SpacexHttpError(Exception):
+    pass
+
+
+class SpacexURLRequired(Exception):
+    pass
+
+
+class SpacexTimeout(Exception):
+    pass
+
+
+def fetch_spacex_data(url):
+    try:
+        response = requests.get('https://api.spacexdata.com/v3/launches')
+        response.raise_for_status()
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError):
+        raise SpacexHttpError('Fail to fetch SpaceX data.')
+    except requests.exceptions.URLRequired:
+        raise SpacexURLRequired('A valid URL is required to make a request.')
+    except (requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
+        raise SpacexTimeout('The request timeout.')
+    return response.json()
+
+
+data = fetch_spacex_data('https://api.spacexdata.com/v3/launches')
+
 
 flights = []
 for row in response.json():
-    flight_number = row.get('flight_number', None)
-    mission_name = row.get('mission_name', None)
-    rocket_id = row.get('rocket_id', None)
-    launch_date_utc = row.get('launch_date_utc', None)
-    video_link = row.get('video_link', None)
+    flight_number = row['flight_number']
+    mission_name = row['mission_name']
+    rocket_id = row['rocket']['rocket_id']
+    launch_date_utc = row['launch_date_utc']
+    video_link = row['links']['video_link']
     flights.append([flight_number, mission_name, rocket_id, launch_date_utc, video_link])
 
 with open("space.csv", 'w') as csvfile:
@@ -19,3 +49,6 @@ with open("space.csv", 'w') as csvfile:
     csvwriter.writerows(flights)
 
 print(flights)
+
+
+
